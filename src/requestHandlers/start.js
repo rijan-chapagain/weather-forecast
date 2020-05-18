f22255d8a96bca05e134699aa03c26930831e3c2
rijan <hrijon.chp@gmail.com>
2020-05-18 09:37:11 +0800

    display index page and store form inputs

---
+++ src/requestHandlers/start.js
@@ -0,0 +1,68 @@
+var fs = require('fs');
+const { parse } = require('querystring');
+var display = require('./display');
+// var view = require('./view');
+
+/**
+ * send status report to server
+ * display html form on client 
+ * generate form with 3 options to choose where to go
+ * 
+ * @param {object} request 
+ * @param {object} response 
+ */
+function reqStart(request, response){
+    console.log("Request handler 'start' was called.");
+
+    fs.readFile('./html/index.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }   
+        response.writeHead( 200, {"Content-Type": "text/html"} );
+        response.write(html);
+        response.end();
+    });
+}
+
+/**
+ * checking the incoming data
+ * send it to requested request handler
+ * 
+ * @param {object} request 
+ * @param {object} response 
+ */
+function reqCheck(request, response){
+    console.log("Request handler 'check' is processing.");
+   
+    function collectRequestData(request, callback) {
+        const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+        if(request.headers['content-type'] === FORM_URLENCODED) {
+            
+            let body = [];
+            
+            request.on('data', (chunk) => {
+                body += chunk.toString(); //convert buffer to string
+            });
+
+            request.on('end', () => {
+                callback(parse(body))
+
+                response.on('error', (err) => {
+                    console.error(err);
+                });
+            });
+        }
+        else{
+            callback(null);
+        }
+    }
+
+    collectRequestData(request, result => {
+        console.log(result);
+        display.reqDisplay(request, response, result);
+        
+    });
+}
+
+exports.reqStart = reqStart;
+exports.reqCheck = reqCheck;
